8e06a2450e2db6d888aff293dfda3f7007e76ad0
amrit <vishal179singh@gmail.com>
2026-08-18 09:07:54 +0530

    call backhell + promisify + concurrent promise using promise.all()

--- js machine coding round/q23.js
+++ js machine coding round/q23.js
@@ -1,12 +1,39 @@
 // Convert a callback-based function into a Promise-based one
+const fs = require('fs')
 
-function getUserId(id) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (id === 1) {
-                resolve(1);
-                return;
-            }
-        });
+fs.readFile("../react-node-interview-prep.md", "utf-8", (err, data1) => {
+    if (err) return console.error("error reading file1:", err)
+    fs.readFile("../senior-js-interview-guide.md", "utf-8", (err, data2) => {
+        if (err) return console.error("error reading file2:", err)
+        console.log(data1, data2);
+    })
+})
+
+function readFilePromise(path) {
+    if (!path) return "path to de deta😭";
+    return new Promise((res, rej) => {
+        fs.readFile(path, "utf-8", (err, data) => {
+            if (err) rej("error occurs")
+            else res(data)
+        })
     })
 }
+
+readFilePromise("../react-node-interview-prep.md")
+    .then(data1 => {
+        return readFilePromise("../senior-js-interview-guide.md")
+            .then(data2 => console.log(data1, data2))
+    })
+    .catch(err => console.error(err));
+async function readAllFiles() {
+    try {
+        const [promise1, promise2] = await Promise.all([
+            readFilePromise("../react-node-interview-prep.md", "utf-8"),
+            readFilePromise("../senior-js-interview-guide.md", "utf-8")
+        ])
+        console.log(promise1, promise2);
+    } catch (err) {
+        console.error(err);
+    }
+}
+readAllFiles()
